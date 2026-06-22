aa4cd74d0dbe4255eefcb2d381f6ef41cf54211b
Christoph Stein <christoph.werner.stein@gmail.com>
2026-06-22 13:57:49 +0200

    Implement equals value constraint

--- gospl/constraint/__init__.py
+++ gospl/constraint/__init__.py
@@ -2,3 +2,4 @@ from .interface import Constraint
 from .same_as import SameAs
 from .not_ import Not
 from .less_than import LessThan
+from .equals import Equals
---
+++ gospl/constraint/equals.py
@@ -0,0 +1,47 @@
+from qiskit import QuantumCircuit
+from qiskit.circuit import QuantumRegister, AncillaRegister
+from typing import List, Any
+
+from .interface import Constraint
+from gospl.variable import Variable
+
+
+class Equals(Constraint):
+    variables: List[Variable]
+    value: Any
+
+    def __init__(self, var: Variable, value: Any):
+        super().__init__([var])
+        self.value = value
+
+    def build(self, circuit: QuantumCircuit, variable_registers: List[QuantumRegister], ancilla_register: AncillaRegister, used_ancillas: int, signal_register: AncillaRegister, used_signal_qubits: int) -> QuantumCircuit:
+        assert len(
+            variable_registers) == 1, f"Is constraint requires qubit ids for 1 variable. {len(variable_registers)} were given."
+
+        variable = self.variables[0]
+        variable_register = variable_registers[0]
+
+        try:
+            int_value = variable.allowed.index(self.value)
+        except ValueError:
+            raise ValueError(
+                f"'{self.value}' is not among the allowed values of variable '{variable.name}'.")
+
+        bit_string = bin(int_value)[2:].zfill(len(variable_register))
+
+        for bit_i, bit_value in enumerate(bit_string):
+
+            if bit_value == "0":
+                circuit.x(variable_register[bit_i])
+
+        circuit.mcx(control_qubits=variable_register,
+                    target_qubit=signal_register[used_signal_qubits])
+
+        for bit_i, bit_value in enumerate(bit_string):
+
+            if bit_value == "0":
+                circuit.x(variable_register[bit_i])
+
+    @property
+    def ancilla_count(self) -> int:
+        return 0
