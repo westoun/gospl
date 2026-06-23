3b1b8f693a4a6fe7b81041f1596a536640b45d33
Christoph Stein <christoph.werner.stein@gmail.com>
2026-06-23 14:58:55 +0200

    Remove duplicate constraints

--- examples/sudoku.py
+++ examples/sudoku.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
     sudoku = [
         [1, 3, 4, None],
         [4, 2, None, 3],
-        [2, 1, 3, 4],
+        [None, 1, 3, 4],
         [3, 4, None, 1]
     ]
     print_sudoku(sudoku)
@@ -97,37 +97,28 @@ if __name__ == "__main__":
         row = int(coordinates[0])
         col = int(coordinates[1])
 
-        # Add constraints for entries in same row
-        print("\nStarting row considerations")
-        for col_i in range(len(sudoku)):
+        constraint_values = []
 
+        # Add constraints for entries in same row
+        for col_i in range(len(sudoku)):
             if sudoku[row][col_i] is not None:
-                print(f"{variable.name} != {sudoku[row][col_i]}")
-                Not(Equals(variable, sudoku[row][col_i]))
+                constraint_values.append(sudoku[row][col_i])
             elif col_i > col:
                 variable2 = find_variable(variables, row, col_i)
-                print(f"{variable.name} != {variable2.name}")
-                Not(SameAs(variable, variable2))
+                constraint_values.append(variable2)
 
         # Add constraints for entries in same column
-        print("Starting col considerations")
         for row_i in range(len(sudoku)):
-
             if sudoku[row_i][col] is not None:
-                print(f"{variable.name} != {sudoku[row_i][col]}")
-                Not(Equals(variable, sudoku[row_i][col]))
+                constraint_values.append(sudoku[row_i][col])
             elif row_i > row:
                 variable2 = find_variable(variables, row_i, col)
-                print(f"{variable.name} != {variable2.name}")
-                Not(SameAs(variable, variable2))
+                constraint_values.append(variable2)
 
         # Add constraints for each block
-        print("Starting block considerations")
         block_width = int(math.sqrt(len(sudoku)))
         start_row = math.floor(row / block_width) * block_width
         start_col = math.floor(col / block_width) * block_width
-        print("start row:", start_row)
-        print("start col:", start_col)
         for row_i in range(start_row, start_row + block_width):
 
             # Already covered by same row constraint
@@ -141,12 +132,19 @@ if __name__ == "__main__":
                     continue
 
                 if sudoku[row_i][col_i] is not None:
-                    print(f"{variable.name} != {sudoku[row_i][col_i]}")
-                    Not(Equals(variable, sudoku[row_i][col_i]))
+                    constraint_values.append(sudoku[row_i][col_i])
                 elif row_i > row:
                     variable2 = find_variable(variables, row_i, col_i)
-                    print(f"{variable.name} != {variable2.name}")
-                    Not(SameAs(variable, variable2))
+                    constraint_values.append(variable2)
+
+        # Remove duplicates
+        constraint_values = list(set(constraint_values))
+
+        for value in constraint_values:
+            if type(value) == Variable:
+                Not(SameAs(variable, value)) 
+            else:
+                Not(Equals(variable, value)) 
 
     builder = CircuitBuilder(variables)
 
@@ -183,7 +181,7 @@ if __name__ == "__main__":
         # Reverse measurement bit order as qiskit seems to index bottom up.
         solution = solution[::-1] 
 
-        if count < 0.05 * shots:
+        if count < 0.01 * shots:
             continue
 
         solutions = []
