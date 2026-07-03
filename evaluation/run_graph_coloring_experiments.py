d905a160344e156439cecb79a7eb17aa2db82a96
Christoph Stein <christoph.werner.stein@gmail.com>
2026-07-03 15:13:53 +0200

    Rerun experiments with new builder setup

--- evaluation/run_graph_coloring_experiments.py
+++ evaluation/run_graph_coloring_experiments.py
@@ -9,21 +9,26 @@ color_count = 4
 buffer_qubits = [None, 1, 3, 5]
 tag = None
 
+experiment_setups = [
+    (4, 3, 3, [1, 3, 5, None]),
+    (4, 5, 3, [1, 3, 5, None]),
+    (5, 8, 4, [1, 3, 5, None]),
+]
+
 for seed_i in range(seed_num):
     seed = seed_offset + seed_i
 
-    for node_count in node_counts:
-        for edge_count in edge_counts:
+    for node_count, edge_count, color_count, buffer_qubits in experiment_setups:
 
-            for buffer_qubit_count in buffer_qubits:
+        for buffer_qubit_count in buffer_qubits:
 
-                command = ["python", "graph_coloring.py", "-n",
-                           str(node_count), "-e", str(edge_count), "-c", str(color_count), "-s", str(seed)]
+            command = ["python", "graph_coloring.py", "-n",
+                        str(node_count), "-e", str(edge_count), "-c", str(color_count), "-s", str(seed)]
 
-                if buffer_qubit_count is not None:
-                    command.extend(["-bq", str(buffer_qubit_count)])
+            if buffer_qubit_count is not None:
+                command.extend(["-bq", str(buffer_qubit_count)])
 
-                if tag is not None:
-                    command.extend(["-t", tag])
+            if tag is not None:
+                command.extend(["-t", tag])
 
-                subprocess.run(command)
+            subprocess.run(command)
--- evaluation/run_sudoku_experiments.py
+++ evaluation/run_sudoku_experiments.py
@@ -3,14 +3,18 @@ import subprocess
 seed_num = 30
 seed_offset = 0
 
-variable_counts = [5]
-buffer_qubits = [None, 1, 3, 5, 10]
 tag = None 
+
+experiment_setups = [
+    (2, [1, 3, 5, None]),
+    (4, [1, 3, 5, 10, None]),
+    (5, [1, 3, 5, 10, None])
+]
 
 for seed_i in range(seed_num):
     seed = seed_offset + seed_i
 
-    for variable_count in variable_counts:
+    for variable_count, buffer_qubits in experiment_setups:
         for buffer_qubit_count in buffer_qubits:
             
             command = ["python", "sudoku.py", "-v", str(variable_count), "-s", str(seed)]
